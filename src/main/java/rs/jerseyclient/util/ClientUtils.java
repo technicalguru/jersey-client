2c28866f6c5f03f1b2d3fd949cbdd471c4030d55
Ralph Schuster <ralph.schuster@dlh.de>
2023-03-16 12:10:03 +0100

    Add some useful methods for refrencing generic types.

--- src/main/java/rs/jerseyclient/util/ClientUtils.java
+++ src/main/java/rs/jerseyclient/util/ClientUtils.java
@@ -3,10 +3,14 @@
  */
 package rs.jerseyclient.util;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.logging.LoggingFeature;
@@ -53,4 +57,38 @@ public class ClientUtils {
 		return ClientBuilder.newClient(clientConfig);
 	}
 	
+	/**
+	 * Creates a {@link GenericType} reference for a list of given class.
+	 * <p>This method is intended to be used with JSON (de)serialization.</p>
+	 * @param <T> the type of the items
+	 * @param clazz the class of the items
+	 * @return the {@link GenericType} for a list of these items
+	 */
+	public static <T> GenericType<ArrayList<T>> getListTypeRef(Class<T> clazz) {
+		return new GenericType<ArrayList<T>>() {};
+	}
+	
+	/**
+	 * Creates a {@link GenericType} reference for a set of given class.
+	 * <p>This method is intended to be used with JSON (de)serialization.</p>
+	 * @param <T> the type of the items
+	 * @param clazz the class of the items
+	 * @return the {@link GenericType} for a set of these items
+	 */
+	public static <T> GenericType<Set<T>> getSetTypeRef(Class<T> clazz) {
+		return new GenericType<Set<T>>() {};
+	}
+	
+	/**
+	 * Creates a {@link GenericType} reference for a map of given key and value classes.
+	 * <p>This method is intended to be used with JSON (de)serialization.</p>
+	 * @param <K> type of keys
+	 * @param <V> type of values
+	 * @param keyClass class of keys
+	 * @param valueClass class of values
+	 * @return the {@link GenericType} for a map of these types
+	 */
+	public static <K,V> GenericType<Map<K,V>> getMapTypeRef(Class<K> keyClass, Class<V> valueClass) {
+		return new GenericType<Map<K,V>>() {};
+	}
 }
