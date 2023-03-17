24d8733eead7555c611d097f859390cf7d21d7bb
Ralph Schuster <ralph.schuster@dlh.de>
2023-03-17 16:58:17 +0100

    Refactor for proxy usage

--- src/main/java/rs/jerseyclient/JerseyClient.java
+++ src/main/java/rs/jerseyclient/JerseyClient.java
@@ -3,13 +3,22 @@
  */
 package rs.jerseyclient;
 
-import javax.ws.rs.client.Client;
+import java.util.logging.Level;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.logging.LoggingFeature;
+
+import com.fasterxml.jackson.core.util.JacksonFeature;
 
 import rs.jerseyclient.util.AbstractClient;
 import rs.jerseyclient.util.ClientFilter;
-import rs.jerseyclient.util.ClientUtils;
+import rs.jerseyclient.util.ObjectMapperProvider;
+import rs.jerseyclient.util.ProxyConfig;
 
 
 /**
@@ -28,65 +37,102 @@ public class JerseyClient extends AbstractClient {
 	public static String URL     = "https://github.com/technicalguru/jersey-client";
 	
 	private Client             client;
-	private ClientFilter       filter;
 	private JerseyClientConfig config;
 
 	/**
 	 * Constructor.
-	 * <p>Be aware that the constructor immediately calls {@link #configure(JerseyClientConfig, ClientFilter)} and
+	 * <p>Be aware that the constructor immediately calls {@link #configure(JerseyClientConfig)} and
 	 *    {@link #authorize()}.</p>
 	 * @param config - the config to be used
 	 */
 	public JerseyClient(JerseyClientConfig config) {
-		this(config, null);
-	}
-	
-	/**
-	 * Constructor.
-	 * <p>Be aware that the constructor immediately calls {@link #configure(JerseyClientConfig, ClientFilter)} and
-	 *    {@link #authorize()}.</p>
-	 * @param config - the config to be used
-	 * @param clientFilter - a specific client filter to be used (defaults to {@link ClientFilter} if {@code null})
-	 */
-	public JerseyClient(JerseyClientConfig config, ClientFilter clientFilter) {
 		super();
-		configure(config, clientFilter);
+		configure(config);
 		authorize();
 	}
 
 	/**
-	 * Configures JAX-RS client and main web target.
+	 * Constructor.
+	 * <p>Be aware that the constructor immediately calls {@link #authorize()}.</p>
 	 * @param config - the config to be used
-	 * @param clientFilter - a specific client filter to be used (defaults to {@link ClientFilter} if {@code null})
-	 * @see #createClientConfig()
 	 */
-	protected void configure(JerseyClientConfig config, ClientFilter clientFilter) {
+	public JerseyClient(Client config) {
+		super();
+		authorize();
+	}
+
+	/**
+	 * Configures JAX-RS client and main web target based on this config.
+	 * <p>Calls {@link #createClient()} to actually create the client.</p>
+	 * @param config - the config to be used
+	 * @see #createClient()
+	 */
+	protected void configure(JerseyClientConfig config) {
 		this.config = config;
 		this.client = createClient();
-		this.filter = clientFilter != null ? clientFilter : new ClientFilter(NAME+"/"+VERSION+" ("+URL+")");
-		
 		setTarget(client.target(config.getUri()));
-		getTarget().register(this.filter);
 	}
 	
 	/**
 	 * Creates the Jersey client configuration based on the config for this client.
 	 * <p>Descendants can override this method in order to manipulate the config.</p>
 	 * @return the config to be used for the JAX-WS Jersey client.
-	 * @see ClientUtils#createClientConfig(JerseyClientConfig)
 	 */
 	protected ClientConfig createClientConfig() {
-		return ClientUtils.createClientConfig(config);
+		ClientConfig clientConfig = new ClientConfig();
+		if (config.isVerbose()) {
+			clientConfig.property(LoggingFeature.LOGGING_FEATURE_VERBOSITY_CLIENT, LoggingFeature.Verbosity.PAYLOAD_TEXT);
+			clientConfig.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, Level.INFO.getName());
+		}
+		if (config.getObjectMapper() != null) {
+			ObjectMapperProvider.setMapper(config.getObjectMapper());
+			clientConfig.register(ObjectMapperProvider.class);
+			clientConfig.register(JacksonFeature.class);
+		}
+		ProxyConfig proxyConfig = config.getProxyConfig();
+		if ((proxyConfig != null) && (proxyConfig.getProxyHost() != null)) {
+			clientConfig.connectorProvider(new ApacheConnectorProvider());
+			clientConfig.property(ClientProperties.PROXY_URI, "http://"+proxyConfig.getProxyHost()+":"+proxyConfig.getProxyPort());
+			if (proxyConfig.getUsername() != null) {
+				clientConfig.property(ClientProperties.PROXY_USERNAME, proxyConfig.getUsername());
+				clientConfig.property(ClientProperties.PROXY_PASSWORD, proxyConfig.getPassword());
+			}
+		}
+
+		return clientConfig;
 	}
 
 	/**
 	 * Creates the actual JAX-WS Jersey client instance.
 	 * <p>Desecendants can override this method when they want full control over the creation of the client.</p>
+	 * <p>Also calls {@link #configure(Client)}.</p>
 	 * @return the client
 	 * @see #createClientConfig()
+	 * @see #configure(Client)
 	 */
 	protected Client createClient() {
-		return ClientUtils.createClient(createClientConfig(), config.getProxyConfig());
+		Client rc = ClientBuilder.newClient(createClientConfig());
+		configure(rc);
+		return rc;
+	}
+	
+	/**
+	 * Configures the client by setting the default auth filter and - if applicable - the
+	 * proxy filter.
+	 * @param client the client to configure
+	 * @see #registerAuthFilter(Client)
+	 */
+	protected void configure(Client client) {
+		registerAuthFilter(client);
+	}
+	
+	/**
+	 * Registers the default filter(s) for authorization.
+	 * @param client the client to be configured
+	 * @see ClientFilter
+	 */
+	protected void registerAuthFilter(Client client) {
+		client.register(new ClientFilter(NAME+"/"+VERSION+" ("+URL+")"));
 	}
 	
 	/**
@@ -95,14 +141,6 @@ public class JerseyClient extends AbstractClient {
 	 */
 	public Client getClient() {
 		return client;
-	}
-
-	/**
-	 * Returns the filter applied to the Jersey client.
-	 * @return the filter
-	 */
-	public ClientFilter getFilter() {
-		return filter;
 	}
 
 	/**
--- src/main/java/rs/jerseyclient/util/ClientUtils.java
+++ src/main/java/rs/jerseyclient/util/ClientUtils.java
@@ -6,19 +6,8 @@ package rs.jerseyclient.util;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
-
-import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.HttpUrlConnectorProvider;
-import org.glassfish.jersey.logging.LoggingFeature;
-
-import com.fasterxml.jackson.core.util.JacksonFeature;
-
-import rs.jerseyclient.JerseyClientConfig;
 
 /**
  * Some useful methods.
@@ -28,80 +17,6 @@ import rs.jerseyclient.JerseyClientConfig;
  */
 public class ClientUtils {
 
-	/**
-	 * Create a JAX-WS client.
-	 * 
-	 * @param verbose - whether verbosity shall be enabled.
-	 * @return the client created
-	 */
-	public static Client createClient(boolean verbose) {
-		JerseyClientConfig config = new JerseyClientConfig();
-		config.setVerbose(verbose);
-		return createClient(createClientConfig(config), config.getProxyConfig());
-	}
-	
-	/**
-	 * Create a JAX-WS client config based on the jersey client config.
-	 * 
-	 * @param config - the configuration to be used
-	 * @return the client configuration created
-	 * @see #applyProxyConfig(ClientConfig, ProxyConfig)
-	 */
-	public static ClientConfig createClientConfig(JerseyClientConfig config) {
-		ClientConfig clientConfig = new ClientConfig();
-		if (config.isVerbose()) {
-			clientConfig.property(LoggingFeature.LOGGING_FEATURE_VERBOSITY_CLIENT, LoggingFeature.Verbosity.PAYLOAD_TEXT);
-			clientConfig.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, Level.INFO.getName());
-		}
-		if (config.getObjectMapper() != null) {
-			ObjectMapperProvider.setMapper(config.getObjectMapper());
-			clientConfig.register(ObjectMapperProvider.class);
-			clientConfig.register(JacksonFeature.class);
-		}
-		ProxyConfig proxyConfig = config.getProxyConfig();
-		if ((proxyConfig != null) && (proxyConfig.getProxyHost() != null)) {
-			applyProxyConfig(clientConfig, proxyConfig);
-		}
-
-		return clientConfig;
-	}
-	
-	/**
-	 * Configures the proxy based on the given proxy.
-	 * <p>This method cannot apply authentication.</p>
-	 * @param config - the Jersey config to change
-	 * @param proxyConfig - the proxy configuration
-	 * @see #applyProxyConfig(Client, ProxyConfig)
-	 */
-	public static void applyProxyConfig(ClientConfig config, ProxyConfig proxyConfig) {
-		config.connectorProvider(new HttpUrlConnectorProvider().connectionFactory(new ProxyConnectionFactory(proxyConfig)));
-	}
-
-	/**
-	 * Create a JAX-WS client based on the client config.
-	 * @param config the Jersey client config
-	 * @return the client created
-	 */
-	public static Client createClient(ClientConfig config, ProxyConfig proxyConfig) {
-		Client rc = ClientBuilder.newClient(config);
-		if ((proxyConfig != null) && (proxyConfig.getProxyHost() != null)) {
-			applyProxyConfig(rc, proxyConfig);
-		}
-		return rc;
-	}
-	
-	/**
-	 * Configures the client according to the proxy config.
-	 * <p>This method cannot apply the proxy host itself.</p>
-	 * @param client the client
-	 * @param proxyConfig the proxy configuration
-	 * @see #applyProxyConfig(ClientConfig, ProxyConfig)
-	 */
-	public static void applyProxyConfig(Client client, ProxyConfig proxyConfig) {
-		if (proxyConfig.getUsername() != null) {
-			client.register(new ProxyAuthFilter(proxyConfig.getUsername(), proxyConfig.getPassword()));
-		}
-	}
 	/**
 	 * Creates a {@link GenericType} reference for a list of given class.
 	 * <p>This method is intended to be used with JSON (de)serialization.</p>
